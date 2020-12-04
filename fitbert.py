117c5d7151d7fa721a364a6b1ea9472a1437c17d
Hamid Ebadi <hamid.ebadi@gmail.com>
2020-12-04 20:09:38 +0100

    Fitbert to multiFitbert

--- fitbert.py
+++ fitbert.py
@@ -14,10 +14,9 @@ from transformers import (
 model=None
 tokenizer=None
 model_name="bert-large-uncased"
-mask_token="***mask***"
+mask_token="[MASK]"
 disable_gpu=False
-mask_token = mask_token
-device = torch.device( "cpu" )
+device = torch.device("cpu")
 print("device:", device)
 if not model:
     print("using model:", model_name)
@@ -38,40 +37,52 @@ if not tokenizer:
         tokenizer = BertTokenizer.from_pretrained(model_name)
 else:
     tokenizer = tokenizer
+
 bert.eval()
 
 def softmax(x):
     return x.exp() / (x.exp().sum(-1)).unsqueeze(-1)
 
 def rank_single(masked_sent: str, words: List[str]):
-    print("MSG4 :: ", masked_sent, words )
-    pre, post = masked_sent.split(mask_token)
-    print("MSG5 :: ", pre, post )
-    tokens = ["[CLS]"] + tokenizer.tokenize(pre)
-    target_idx = len(tokens)
-    tokens += ["[MASK]"]
-    tokens += tokenizer.tokenize(post) + ["[SEP]"]
+    print("MSG1 :: ", masked_sent, words )
+
+
+    tokenized_text = tokenizer.tokenize(masked_sent)
+    # Mask a token that we will try to predict back with `BertForMaskedLM`
+    masked_index = 7
+    tokenized_text[masked_index] = '[MASK]'
     
-    print("MSG2 :: ", tokens)
+    masked_index_extra = 9
+    tokenized_text[masked_index_extra] = '[MASK]'
+    
+    print("MSG2 :: ", masked_sent)
+    print("MSG3 :: ", tokenized_text)
+
 
     words_ids = (
         seq(words)
         .map(lambda x: tokenizer.tokenize(x))
         .map(lambda x: tokenizer.convert_tokens_to_ids(x)[0])
     )
-    print("MSG6 :: ", words_ids)
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
-    print("MSG3 :: ", input_ids)
+    print("MSG4 :: ", words_ids)
+    input_ids = tokenizer.convert_tokens_to_ids(tokenized_text)
+    print("MSG5 :: ", input_ids)
     tens = torch.tensor(input_ids).unsqueeze(0)
     tens = tens.to(device)
-    print("MSG7 :: ", tens)
+    print("MSG6 :: ", tens)
     with torch.no_grad():
         preds = bert(tens)[0]
         probs = softmax(preds)
 
         ranked_pairs = (
             seq(words_ids)
-            .map(lambda x: float(probs[0][target_idx][x].item()))
+            .map(lambda x: float(probs[0][masked_index][x].item()))
+            .zip(words)
+            .sorted(key=lambda x: x[0], reverse=True)
+        )
+        ranked_pairs_extra = (
+            seq(words_ids)
+            .map(lambda x: float(probs[0][masked_index_extra][x].item()))
             .zip(words)
             .sorted(key=lambda x: x[0], reverse=True)
         )
@@ -80,52 +91,32 @@ def rank_single(masked_sent: str, words: List[str]):
         ranked_options = (seq(ranked_pairs).map(lambda x: x[1])).list()
         ranked_options_prob = (seq(ranked_pairs).map(lambda x: x[0])).list()
 
-        print("MSG8 :: ", ranked_options)
-        print("MSG9 :: ", ranked_options_prob)
+        print("MSG6 :: ", ranked_options)
+        print("MSG7 :: ", ranked_options_prob)
+        
+        ranked_options_extra = (seq(ranked_pairs_extra).map(lambda x: x[1])).list()
+        ranked_options_prob_extra = (seq(ranked_pairs_extra).map(lambda x: x[0])).list()
+        
+        print("MSG8 :: ", ranked_options_extra)
+        print("MSG9 :: ", ranked_options_prob_extra)
+        
 
-        del tens, preds, probs, tokens, words_ids, input_ids
+        del tens, preds, probs, tokenized_text, words_ids, input_ids
         if device == "cuda":
             torch.cuda.empty_cache()
         return ranked_options, ranked_options_prob
 
 
+sent = "Why Bert, you're looking test today!"
+options = ['buff', 'handsome', 'strong']
 
-def rank(
-    sent: str,
-    options: List[str],
-    with_prob: bool = False,
-    ):
-    """
-    Rank a list of candidates
+ranked, prob = rank_single(sent, options)
 
-    returns: Either a List of strings,
-    or if `with_prob` is True, a Tuple of List[str], List[float]
-
-    """
-
-    options = ['buff', 'handsome', 'strong']
-    sent = "Why Bert, you're looking ***mask*** today!"
-    
-    print("MSG1 ::", options, sent)
-
-    ranked, prob = rank_single(sent, options)
-
-    ranked = (
+ranked = (
         seq(ranked)
         .map(lambda x: seq(x).make_string(" ").strip())
         .list()
     )
-    if with_prob:
-        return ranked, prob
-    else:
-        return ranked
+    
 
-
-
-masked_string = "Why Bert, you're looking ***mask*** today!"
-options = ['buff', 'handsome', 'strong']
-
-ranked_options = rank(masked_string, options=options, with_prob= True)
-# >>> ['handsome', 'strong', 'buff']
-
-print(ranked_options)
+print(ranked, prob)
