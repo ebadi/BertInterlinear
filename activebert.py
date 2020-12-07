4428822d48a1fcbe0f39e2d62533ec7b8c086b92
Hamid Ebadi <hamid.ebadi@gmail.com>
2020-12-07 15:49:32 +0100

    Assigning probability to each gap option

--- activebert.py
+++ activebert.py
@@ -52,7 +52,7 @@ tokenized_sentence = sentence.split()
 sentecelist = []
 bertlist = []
 bertlist_ids = []
-masked_sentecelist = []
+masked_sentencelist = []
 
 print("\n\n======== Sentecelist::")
 for i in range(len(tokenized_sentence)):
@@ -61,9 +61,9 @@ for i in range(len(tokenized_sentence)):
     toklist_id = tokenizer.convert_tokens_to_ids(toklist)
     
     if i % 5 == 1 :
-    	trans_options =  ["brown", tokenized_sentence[i], "dog", "lazy"]
+    	trans_options =  [("brown",0), (tokenized_sentence[i],0), ("dog",0), ("lazy",0)]
     else:
-    	trans_options = [tokenized_sentence[i]]
+    	trans_options = [(tokenized_sentence[i],1)]
 
     sentecelist.append((i, compound_word, (toklist, toklist_id), trans_options ))
     print(sentecelist[i])
@@ -73,17 +73,17 @@ for i in range(len(tokenized_sentence)):
 for i in range(len(sentecelist)):
     (indx, word, tokz , trans_options) = sentecelist[i]
     if len(trans_options) == 1:
-        masked_sentecelist.append((indx, word, tokz , trans_options))
+        masked_sentencelist.append((indx, word, tokz , trans_options))
     else:
-        masked_sentecelist.append((indx, "[MASK]", ([mask_token], [mask_token_id] ) , trans_options))
+        masked_sentencelist.append((indx, "[MASK]", ([mask_token], [mask_token_id] ) , trans_options))
 
-print("\n\n======== Masked_sentecelist::")
-for i in range(len(masked_sentecelist)):
-    (indx, word, tokz , trans_options) = masked_sentecelist[i]
+print("\n\n======== masked_sentencelist::")
+for i in range(len(masked_sentencelist)):
+    (indx, word, tokz , trans_options) = masked_sentencelist[i]
     (toklist, toklist_id) = tokz
     bertlist.extend(toklist)
     bertlist_ids.extend(toklist_id)
-    print(masked_sentecelist[i])
+    print(masked_sentencelist[i])
 
 
 print("\n\n============= BERT")    
@@ -98,34 +98,42 @@ bertIndx = 0
 
 
 sorted_sentecelist = []
+ranked_pairs = []
 
-for i in range(len(masked_sentecelist)):
-    (indx, word, tokz , trans_options) = masked_sentecelist[i]
+for i in range(len(masked_sentencelist)):
+    (indx, word, tokz, trans_options) = masked_sentencelist[i]
     (toklist,toklist_id) = tokz
-    trans_options_ids = (
+    trans_words = (
         seq(trans_options)
+        .map(lambda x: x[0])
+    )
+    trans_options_ids = (
+        seq(trans_words)
         .map(lambda x: tokenizer.tokenize(x))
         .map(lambda x: tokenizer.convert_tokens_to_ids(x)[0])
     )
-
-    #print("Masked_sentecelist index",i,tokz, " Bert index", bertIndx, bertlist[indx] )
+    #print("masked_sentencelist index",i,tokz, " Bert index", bertIndx, bertlist[indx] )
     if bertlist_ids[bertIndx] == mask_token_id :
         ranked_pairs = (
             seq(trans_options_ids)
             .map(lambda x: float(probs[0][i][x].item()))
-            .zip(trans_options)
+            .zip(trans_words)
+            .map (lambda x: (x[1], x[0]))
             .sorted(key=lambda x: x[0], reverse=True)
         )
-        #print(i, "ranked_pairs: " , ranked_pairs)
-        print("[" + ranked_pairs[0][1] + "]",  end=" ")
-        ranked_options = (seq(ranked_pairs).map(lambda x: x[1])).list()
-        #print(i, "ranked_options: " , ranked_options)
+        print("[" + ranked_pairs[0][0] + "]",  end=" ")
     else:
+        ranked_pairs = trans_options
         print(word, end=" ")
-        
     bertIndx = bertIndx + len(toklist_id)
+    sorted_sentecelist.append((indx, word, tokz , ranked_pairs))
 
 
+
+print("\n\n======== sorted_sentecelist::")
+for i in range(len(sorted_sentecelist)):
+    print(sorted_sentecelist[i])
+    
 
 
 
